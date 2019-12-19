c993ee05477070e01f935170e99206aa966d6f53
github username <github email address>
2019-12-19 05:09:34 -0800

    added character class in file character.js:cross file use works

---
+++ javascript/Character.js
@@ -0,0 +1,12 @@
+// character creation and use code
+class Character {
+
+    // characters need a name
+    constructor(name){
+        this.name = name;
+    }
+
+    read(){
+        console.log(this.name);
+    }
+}
--- javascript/index.js
+++ javascript/index.js
@@ -7,10 +7,20 @@ window.addEventListener("DOMContentLoaded", ()=>{
 // also redirect
 function gameStart(){
     const game = document.getElementById('game');
-    let greeting = document.createElement('p');
+    let greeting = document.createElement('h1');
     greeting.innerText = "Welcome to my (Jared Burnham) first ever textbased RPG click here to get started"
     game.appendChild(greeting);
     let start = document.createElement('button');
     start.innerText = "Click Me";
+    start.addEventListener("click", ()=>{
+        startCharacterCreation()
+    });
+    game.appendChild(start);
+}
 
+//character creation setup
+// uses character Class
+function startCharacterCreation(){
+    let j = new Character('Jareluped');
+    j.read()
 }
