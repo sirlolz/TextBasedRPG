00f45b3b5803b8a9bf82247225a6be02b0fc311a
github username <github email address>
2019-12-19 21:51:13 -0800

    chracter creation form made

--- javascript/clearText.js
+++ javascript/clearText.js
@@ -1,4 +1,5 @@
+// clears the id='game' div
+// should be used for screen transitions
 function clearText(){
-    game.innerText = ""
-    console.log(game);
+    game.innerText = "";
 }
--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded", ()=>{
 });
 
 // Game greeting
-// also redirect
+// along with game start
 function gameStart(){
     const game = document.getElementById('game');
     let greeting = document.createElement('h1');
@@ -14,7 +14,16 @@ function gameStart(){
     start.innerText = "Click Me";
     start.addEventListener("click", ()=>{
         clearText();
-        // startCharacterCreation();
+        startCharacterCreation();
     });
     game.appendChild(start);
+}
+
+function startCharacterCreation(){
+    const characterForm = document.createElement('form');
+    const nameInput = document.createElement('textarea');
+    const submit = document.createElement('submit');
+    characterForm.appendChild(nameInput);
+    characterForm.appendChild(submit);
+    game.appendChild(characterForm);
 }
