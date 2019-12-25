2695886d79aef868a4951eb71937bcd0509359a4
Jared Burnham <51872722+sirlolz@users.noreply.github.com>
2019-12-25 07:15:40 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// javascript inserton point
+window.addEventListener("DOMContentLoaded", ()=>{
+    gameStart();
+});
+
+let greeting = document.createElement('h1');
+
+// Game greeting
+// along with game start
+function gameStart(){
+    const game = document.getElementById('game');
+    greeting.innerText = "Welcome to my (Jared Burnham) first ever textbased RPG click here to get started"
+    game.appendChild(greeting);
+    const start = document.createElement('button');
+    start.innerText = "Click Me";
+    start.addEventListener("click", ()=>{
+        clearText();
+        startCharacterCreation();
+    });
+    game.appendChild(start);
+}
+
+//start of player journey
+// player character has been created
+// file charactercreation
+function playerStart(){
+    clearText();
+    greeting.innerText = "welcome to The Land of Mysterie";
+    game.appendChild(greeting);
+    cleanStatDisplay(player);
+}
